7923b074434a091a6f74fa429610a52477aab01a
Sobarzo14 <abelardo.sobarzo.14@gmail.com>
2025-05-16 19:09:55 -0400

    Created the expo router

---
+++ app/(tabs)/partners.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class Partners extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Partners</Text>
+      </View>
+    );
+  }
+}
