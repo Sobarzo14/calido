f856e253ea1053ba1cfa96399ad7851566981118
Sobarzo14 <63866666+Sobarzo14@users.noreply.github.com>
2025-06-01 00:02:39 -0400

    Added a link to the settings page

--- app/(tabs)/(profile)/_layout.tsx
+++ app/(tabs)/(profile)/_layout.tsx
@@ -1,17 +1,20 @@
-import { Stack } from "expo-router";
-import React, { Component } from "react";
+import { Stack, useNavigation } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 
-export default class ProfileLayout extends Component {
-  render() {
-    return (
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerTitle: "Profile",
-          }}
-        />
-      </Stack>
-    );
-  }
+export default function ProfileLayout() {
+  const navigation = useNavigation()
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Profile",
+          headerRight: () => <TouchableOpacity onPress={() => navigation.navigate('SettingsScreen' as never)} style={{ marginRight: 15 }}>
+            <Text>Go</Text>
+          </TouchableOpacity>
+        }}
+      />
+    </Stack>
+  );
 }
